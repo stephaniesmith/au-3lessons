3b9257a966bf5f8364a98d28f8c60c3f0564a7e6
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-06-12 08:40:09 -0700

    working on selectors

--- src/app/courses/course.selectors.ts
+++ src/app/courses/course.selectors.ts
@@ -2,13 +2,17 @@ import {createFeatureSelector, createSelector} from '@ngrx/store';
 import {CoursesState} from './course.reducers';
 
 import * as fromCourse from './course.reducers';
+import * as fromLessons from './lessons.reducers';
+import { PageQuery } from './course.actions';
+import { LessonsState } from './lessons.reducers';
 
-export const selectCoursesState = createFeatureSelector<CoursesState>("courses");
+export const selectCoursesState = createFeatureSelector<CoursesState>('courses');
+export const selectLessonsState = createFeatureSelector<LessonsState>('lessons');
 
 
 
 
-export const selectCourseById = (courseId:number) => createSelector(
+export const selectCourseById = (courseId: number) => createSelector(
   selectCoursesState,
   coursesState => coursesState.entities[courseId]
 );
@@ -17,11 +21,25 @@ export const selectCourseById = (courseId:number) => createSelector(
 export const selectAllCourses = createSelector(
   selectCoursesState,
   fromCourse.selectAll
-
 );
 
 
 export const allCoursesLoaded = createSelector(
   selectCoursesState,
   coursesState => coursesState.allCoursesLoaded
+);
+
+export const selectAllLessons = createSelector(
+  selectLessonsState,
+  fromLessons.selectAll
+);
+
+export const selectLessonsPage = (courseId: number, { pageIndex, pageSize }: PageQuery) => createSelector(
+  selectAllLessons,
+  allLessons => {
+    const start = pageIndex * pageSize;
+    const end = start + pageSize;
+
+    return allLessons.filter(lesson => lesson.courseId === courseId).slice(start, end);
+  }
 );
